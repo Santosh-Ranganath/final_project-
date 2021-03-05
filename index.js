f869f3fa6e105ffba3ebdeae233dbc834082889b
bes8888 <70267734+bes8888@users.noreply.github.com>
2021-03-05 16:13:12 -0600

    commented index.js with what needs done

--- index.js
+++ index.js
@@ -1,7 +1,45 @@
 firebase.auth().onAuthStateChanged(async function(user) {
+      
+
+
   if (user) {
     // Signed in
     console.log('signed in')
+
+    // direct traffic based on who logged in
+        // if user doesn't have a pending order, direct them to the order page
+        // if user has a pending order, direct them to that page
+
+
+
+    // order page: Add a bunch of event listeners
+
+        // Log out button
+              // executes some firebase code to logout
+
+
+        // order buttons (could probably do this with 1 section of code with a tweak based on premium vs std)
+              // stores values from html inputs on the course and hole # 
+              // also logs the current time
+              // lambda function later: send these things to firebase
+              // move to pending order page (or reformat index.html to look that way)
+
+    
+    // on the order pending page...
+        // go get details on the current order
+              // lambda function:  pull outstanding order for that user from firebase
+              // update html to display order details and wait time
+        
+        // add event listeners to buttons
+            // log out button w/ firebase code
+            // cancel button
+                // grab current order info
+                // send to firebase w/ update on "status" to cancelled
+                // move back to order page
+            // refresh?  a button to refresh from firebase, or we code in an autorefresh once/min?
+
+  
+
   } else {
     // Signed out
     console.log('signed out')
@@ -19,5 +57,13 @@ firebase.auth().onAuthStateChanged(async function(user) {
 
     // Starts FirebaseUI Auth
     ui.start('.sign-in-or-sign-out', authUIConfig)
+
+    // we will need to insert some HTML here in order to add our logo/customize the login screen
+
+
+
   }
 })
+
+
+// Any functions created go below this line (outside the firebase stuff)
