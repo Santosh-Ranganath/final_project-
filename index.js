db64b879854e9e769175305671cc0e541834e7bd
bes8888 <70267734+bes8888@users.noreply.github.com>
2021-03-06 13:46:54 -0600

    saturday updates

--- index.js
+++ index.js
@@ -1,41 +1,106 @@
 firebase.auth().onAuthStateChanged(async function(user) {
       
-
+  let db = firebase.firestore()
 
   if (user) {
     // Signed in
     console.log('signed in')
 
+      
     // direct traffic based on who logged in
         // if user doesn't have a pending order, direct them to the order page
         // if user has a pending order, direct them to that page
 
-
-
     // order page: Add a bunch of event listeners
 
         // Log out button
-              // executes some firebase code to logout
-
+              // Put the button in an empty div created for it
+              document.querySelector('.sign-in-or-sign-out').innerHTML = `
+                <button class="text-pink-500 underline sign-out">Sign Out</button>
+              `
+              // event listener to actually sign out when clicked
+              document.querySelector('.sign-out').addEventListener('click', function(event) {
+                console.log('sign out clicked')
+                firebase.auth().signOut()
+                document.location.href = 'index.html'
+              })
 
         // order buttons (could probably do this with 1 section of code with a tweak based on premium vs std)
-              // stores values from html inputs on the course and hole # 
-              // also logs the current time
-              // lambda function later: send these things to firebase
-              // move to pending order page (or reformat index.html to look that way)
+              // stores values from html inputs on the course and hole #, and current time
+                
+              let std_order = document.querySelector('.std_order')
+              std_order.addEventListener('submit', async function(event){
+                let course = document.querySelector('.course').value
+                let hole = document.querySelector('hole').value
+                // will need to add error handling later if no course/hole selected
+                let ordertime = firebase.firestore.FieldValue.serverTimestamp()
+                let priority = 'standard'
+                let userID = user.uid
+                let status = 'pending'
 
-    
+                // send this info to firebase (move to lambda function later)
+
+                let order = {
+                  course: course,
+                  hole: hole,
+                  ordertime: ordertime,
+                  priority: priority,
+                  userID: userID,
+                  status: status,
+                  promisetime: ordertime + 15
+                  // add some complexity later for dynamic wait times based on order volume?
+                }
+                
+                let docRef =  await db.collection('orders').add(order)
+                let ordernum = docRef.id
+
+                // move to a pending order screen
+                // this refreshes page... maybe divert to separate html page?
+                document.location.href = 'index.html'
+              })
+ 
+            
     // on the order pending page...
         // go get details on the current order
               // lambda function:  pull outstanding order for that user from firebase
+              let querySnapshot = await db.collection('orders')
+              .where('userId', '==', user.uid)
+              .where('status', '==', 'pending')
+              .get()
+
+              let orderdetails = querySnapshot.docs
+              let timeleft = orderdetails.promisetime - firebase.firestore.FieldValue.serverTimestamp()            
+              
               // update html to display order details and wait time
+              document.querySelector('.orderdetails').insertAdjacentHTML('beforeend', `
+              <div>
+                  <div>
+                    Wait time: ${timeleft} Minutes
+                    </div>
+              </div>`)
         
         // add event listeners to buttons
             // log out button w/ firebase code
+                  // Put the button in an empty div created for it
+                  document.querySelector('.sign-in-or-sign-out').innerHTML = `
+                  <button class="text-pink-500 underline sign-out">Sign Out</button>
+                  `
+                  // event listener to actually sign out when clicked
+                  document.querySelector('.sign-out').addEventListener('click', function(event) {
+                  console.log('sign out clicked')
+                  firebase.auth().signOut()
+                  document.location.href = 'index.html'
+                  })
             // cancel button
                 // grab current order info
+                    // should already be stored at this point...
                 // send to firebase w/ update on "status" to cancelled
+                      await db.collection('orders').update({
+                        status: 'cancelled'
+                      })
+
                 // move back to order page
+                    document.location.href = 'index.html'
             // refresh?  a button to refresh from firebase, or we code in an autorefresh once/min?
 
   
--- netlify/functions/firebase.js
+++ netlify/functions/firebase.js
@@ -1,7 +1,12 @@
 const firebase = require("firebase/app")
 require("firebase/firestore")
 
-const firebaseConfig = {} // replace
+const firebaseConfig = {apiKey: "AIzaSyANVXBOsL8Oij3vllP5r7j85ZM1h69TRKQ",
+authDomain: "kiei-451-9dc69.firebaseapp.com",
+projectId: "kiei-451-9dc69",
+storageBucket: "kiei-451-9dc69.appspot.com",
+messagingSenderId: "276054356383",
+appId: "1:276054356383:web:ec794fb4737cbd02704daa"} // replace
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig)
